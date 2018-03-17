ab0dadbc3f47548286c27a601d9fad994288ab0b
Daniel Phillips <danieledward.phillips@gmail.com>
2018-03-17 14:38:32 -0600

    Add routes for twilio post and get

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post "/messages", to: 'twilio#index'
+  get "/messages", to: 'twilio#index'
+
 end
