3980e700a58c5860cb7dd5840fee18f6463f3e21
Daniel Phillips <danieledward.phillips@gmail.com>
2018-04-14 13:27:47 -0600

    Add breathing room for routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/messages", to: 'twilio#index'
+
   post "/messages", to: 'twilio#create'
+
   get "/inbox", to: 'inbox#index'
+
 end
